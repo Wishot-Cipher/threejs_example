d75027e26c3a212d8d98d48d3998050dc88c219d
Alom Wisdom <ogwisdom27@gmail.com>
2023-10-01 19:46:28 +0100

    made the avater to move from left to right etc

--- main.js
+++ main.js
@@ -1,74 +1,123 @@
 import * as THREE from 'three';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+let modelReady = false;
+let isWalking = false;
+let targetPosition = null;
+let object;
+let explanationElement;
 
 window.addEventListener('load', function () {
-    // Create Scene
-    const scene = new THREE.Scene()
+    const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
-    scene.add(new THREE.AxesHelper(5))
+    scene.add(new THREE.AxesHelper(5));
 
-    // Add a light
-    const light = new THREE.PointLight(0xffffff, 1000)
-    light.position.set(2.5, 7.5, 15)
-    scene.add(light)
+    const camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.1, 10);
+    camera.position.set(0, 0.5, 2);
+    camera.rotation.set(-Math.PI / 8, 0, 0);
 
-    // Add a camera
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.set(0.8, 1.4, 1.0)
-
-    // Add renderer and put in the DOM
     const renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setSize( 200, 200 );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(800, 600);
 
-    var container = document.getElementById( 'canvas' );
-    container.appendChild( renderer.domElement );
+    var container = document.getElementById('canvas');
+    container.appendChild(renderer.domElement);
 
-    // Add the canvas to the DOM
-    // document.body.appendChild( renderer.domElement );
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.target.set(0, 1, 0);
 
-    // Add the orbit controls
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = true
-    controls.target.set(0, 1, 0)
+    var mixer;
 
-    var mixer
-    var modelReady = false
-
-    // Load our FBX model from the directory
     var loader = new FBXLoader();
-    loader.load("Walking.fbx", function(object) {
+    loader.load("Walking.fbx", function(modelObject) {
+        object = modelObject;
+        object.scale.set(0.005, 0.005, 0.005);
+        object.position.set(0, 0, 0);
 
-        // Scale and position the model
-        object.scale.set(0.007, 0.007, 0.007)
-        object.position.set(0, 0, 0)
-
-        // Start the default animation
         mixer = new THREE.AnimationMixer(object);
         var action = mixer.clipAction(object.animations[0]);
         action.play();
 
-        // Add it to the scene
+        object.traverse(function(node) {
+            if (node.isMesh) {
+                node.material.transparent = true;
+                node.material.opacity = 0.5;
+            }
+        });
+
         scene.add(object);
 
-        modelReady = true
-
+        modelReady = true;
     });
 
+    explanationElement = document.getElementById('explanation');
 
-    // Add animation routine
-    var clock = new THREE.Clock();
-    function animate() {
-        requestAnimationFrame( animate );
+    const buttonPositions = [
+        new THREE.Vector3(0, 1.2, -1), // Button 1 (Left)
+        new THREE.Vector3(0, -0.5, 1),  // Button 2 (Right),
+        new THREE.Vector3(-1.5, 0, 0),// Button 3 (Down)
+        new THREE.Vector3(1.5, 0, 0),   // Button 4 (Up)
+    ];
 
-        // Call the animate on the objec
-        if (modelReady) mixer.update(clock.getDelta());
+    for (let i = 0; i < 4; i++) {
+        document.getElementById(`button${i + 1}`).addEventListener('click', function () {
+            if (modelReady && !isWalking) {
+                if (targetPosition) {
+                    mixer.uncacheAction(mixer.clipAction(object.animations[0]));
+                }
+                targetPosition = buttonPositions[i];
+                isWalking = true;
 
-        renderer.render( scene, camera );
+                // Position the explanation element
+                const modelHeadOffset = new THREE.Vector3(0, 1, 0);
+                const modelHeadPosition = object.position.clone().add(modelHeadOffset);
+                const screenPosition = modelHeadPosition.clone().project(camera);
+
+                explanationElement.style.top = `${(screenPosition.y + 1) * window.innerHeight / 2}px`;
+                explanationElement.style.left = `${(screenPosition.x + 1) * window.innerWidth / 2}px`;
+
+                explanationElement.textContent = `Model is walking to Button ${i + 1}.`;
+                explanationElement.style.display = 'block';
+            }
+        });
     }
 
+    function animate() {
+        requestAnimationFrame(animate);
 
+        if (modelReady) {
+            mixer.update(clock.getDelta());
+
+            if (isWalking && targetPosition) {
+                const step = 0.01;
+                const currentPosition = object.position.clone();
+                const direction = new THREE.Vector3();
+                direction.subVectors(targetPosition, currentPosition).normalize().multiplyScalar(step);
+
+                object.position.add(direction);
+
+                const distance = currentPosition.distanceTo(targetPosition);
+                if (distance < step) {
+                    isWalking = false;
+                    targetPosition = null;
+                    explanationElement.style.display = 'none';
+                }
+
+                // Update explanation element position with the model's head
+                const modelHeadOffset = new THREE.Vector3(0, 1, 0);
+                const modelHeadPosition = object.position.clone().add(modelHeadOffset);
+                const screenPosition = modelHeadPosition.clone().project(camera);
+
+                explanationElement.style.top = `${(screenPosition.y + 1) * window.innerHeight / 2}px`;
+                explanationElement.style.left = `${(screenPosition.x + 1) * window.innerWidth / 2}px`;
+            }
+        }
+
+        renderer.render(scene, camera);
+    }
+
+    var clock = new THREE.Clock();
     animate();
 });
